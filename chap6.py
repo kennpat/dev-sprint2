5c86d86b6345a6b9c2664949508f8522b13f1c67
kennpat <kennedypr@gmail.com>
2013-11-14 21:03:05 -0500

    my comments

--- chap6.py
+++ chap6.py
@@ -1,9 +1,81 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Patrick Kennedy
 
 
 # Ex. 6.6
+#returns the first letter
+def first(word):
+	return word[0]
+#returns the last letter
+def last(word):
+	return word[-1]
+#returns everything in between first and last
+def middle(word):
+	return word[1:-1]
+
+
+#function to determine if a palendrome is present
+def is_palindrome(word):
+	length = len(word)
+	
+	if (first(word) == last(word)):
+		#print 'we have a palindrome!'
+		return True
+	else:			
+		#print 'Its not a palindrome!'
+		return False
+	return is_palindrome(middle(word))
+	
+def Test_Run(word): #this was to test the below items individually through the functions
+	print first(word)
+	print last(word)
+	print middle(word)
+
+passIn = 'Baboon'
+passIn2 = 'redivider'
+test1 = '' #results in an error
+test2 = 'B' #no issues, but the last and first only print out and middle does not generate
+test3 = 'CC' #no issues, middle does not print
+
+#Test_Run(test2)
+#length = len(passIn2)
+#print length
+#print passIn2[1:-1]
+
+print is_palindrome(passIn)
+print is_palindrome(passIn2)
+print is_palindrome(test2)
+print is_palindrome(test3)
 
 
 
 # Ex 6.8
+
+#greatest common denominator/divisor/whatever
+
+#courtesy of Wikipedia:
+def gcd(a, b):
+	if (b == 0):
+		return a
+	else:
+		return gcd (b, (a%b))
+	
+
+
+
+
+print "Now for the Greatest Common Divisor"
+print gcd(100, 101)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- chap7.py
+++ chap7.py
@@ -1,9 +1,42 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name: Patrick Kennedy
 
 
 # Ex. 7.5
 
+import math
+
+#thank god for discrete math
+def combinatoric(x):
+	if x == 0:
+		return 1
+	else:
+		holder = combinatoric(x-1)
+		passback = x + holder
+		return passback
+
+def closeTopie():
+	#grabbed this from wikipedia.org/wiki/Pi
+	total = 0
+	k = 0
+	counter = 0
+	factor = 2*math.sqrt(2)/9801
+	while True:
+		num = combinatoric(4*k)*(1103+26390*k)
+		den = combinatoric(k)**4 * 396**(4*k)
+		term = factor * num/den
+		total += term
+		counter = counter +1
+		if abs(term) < 1e-15: break
+		k += 1
+	print counter	#prints the number of loops
+	return 1/total
+
+#this prints the calc and gets us close to Pi
+print closeTopie()
+
+
 
 
 # How many iterations does it take to converge?
+# I think it takes 4 passes
